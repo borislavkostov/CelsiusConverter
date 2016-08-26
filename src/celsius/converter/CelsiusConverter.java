56b268f391b01bd139ae5aad887c25ecc1faf009
borislavkostov <borislavkostov97@gmail.com>
2016-08-26 11:49:32 +0300

    First commit

---
+++ src/celsius/converter/CelsiusConverter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celsius.converter;
+
+/**
+ *
+ * @author templars1914
+ */
+public class CelsiusConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConvertorForm form= new ConvertorForm();
+        form.setVisible(true);
+    }
+    
+}
